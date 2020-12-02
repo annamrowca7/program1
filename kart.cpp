675f1c1747ad71c3535b17de1ac52f8330951cee
annamrowca7 <74540889+annamrowca7@users.noreply.github.com>
2020-12-02 09:45:25 +0100

    Update kart.cpp

--- kart.cpp
+++ kart.cpp
@@ -7,6 +7,7 @@ int main()
     std::default_random_engine e1(r());
     std::uniform_int_distribution<int> uniform_dist(1, 6);
     int n = uniform_dist(e1);
-    return n;
+    cout << n;
+    return 0;
 }
 
