23d7c480f5adc9c77ea17d20cd133e5766a17c6f
annamrowca7 <74540889+annamrowca7@users.noreply.github.com>
2020-12-02 09:04:40 +0100

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <random>
+
+int main()
+{
+    std::random_device r;
+    std::default_random_engine e1(r());
+    std::uniform_int_distribution<int> uniform_dist(1, 6);
+    int n = uniform_dist(e1);
+    return n;
+}
+
